ddb1162b87cbe35a27edb70cf2cbadc180bbcade
Angelina Fabbro <cyborg@digitalocean.com>
2017-08-17 15:58:20 -0400

    I have essentially built a 3D color picker controlled by a leap motion

--- index.js
+++ index.js
@@ -2,15 +2,31 @@ var leap      = require('leapjs')
 var controller  = new leap.Controller()
 controller.connect();
 
-function onServiceStreaming() {
-    console.log("Service started streaming event");
-}
-controller.on('streamingStarted', onServiceStreaming)
+var five = require('johnny-five');
+var board = new five.Board();
 
-function onFrame(frame) {
-  for(var p = 0; p < frame.pointables.length; p++){
-    var pointable = frame.pointables[0]
-    console.log(pointable.stabilizedTipPosition)
+board.on('ready', function() {
+	var redLED = new five.Led(11)
+	var greenLED = new five.Led(10)
+	var blueLED = new five.Led(9)
+
+	redLED.on()
+  greenLED.on()
+  blueLED.on()
+
+  function onServiceStreaming() {
+    console.log('Service started streaming event');
 	}
-}
-controller.on('frame', onFrame)
+	controller.on('streamingStarted', onServiceStreaming)
+
+	function onFrame(frame) {
+	  for(var p = 0; p < frame.pointables.length; p++){
+	    var pointable = frame.pointables[0]
+	    console.log(pointable.stabilizedTipPosition)
+	      redLED.brightness(pointable.stabilizedTipPosition[0])
+  			greenLED.brightness(pointable.stabilizedTipPosition[1])
+  			blueLED.brightness(pointable.stabilizedTipPosition[2])
+		}
+	}
+	controller.on('frame', onFrame)
+});
